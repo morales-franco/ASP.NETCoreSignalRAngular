fabbb38eecdae8647def89a9b1c15dfa25883414
Franco <franco.e.morales@gmail.com>
2017-11-21 14:50:56 -0300

    example completed

---
+++ Server/Server/SignalR/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.InvokeAsync("sendToAll", name, message);
+        }
+    }
+}
